c7621209aeda0a98000077a6173f635e3fdc56af
putii <patrykol1964@gmail.com>
2025-09-07 21:09:29 +0200

    simple form handler

---
+++ contactFormHandler.php
@@ -0,0 +1,16 @@
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $name = htmlspecialchars($_POST["name"]);
+    $mail = htmlspecialchars($_POST["mail"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+    
+    $contactData = "$name|$mail|$subject|$message|".date("Y-m-d H:i:s").
+    "\n";
+
+    if (file_put_contents("messages.txt", $contactData, FILE_APPEND | LOCK_EX) === false) {
+      // error handling could go here
+    };
+  }
+?>
