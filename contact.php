71901237667d89df6019ee0e79171c5869359a91
putii <patrykol1964@gmail.com>
2025-09-08 21:02:39 +0200

    fe

--- contact.php
+++ contact.php
@@ -7,23 +7,35 @@
     <script src="script.js"></script>
   </head>
   <body>
-    <p>Kontakt</p>
-    <form id="form" action="javascript:postData()" method="post">
-      <label for="name">Imię i nazwisko</label>
-      <input id="name" type="text" name="name" placeholder="Imię i nazwisko" required>
+    <header></header>
 
-      <label for="mail">Adres E-mail</label>
-      <input id="mail" type="text" name="mail" placeholder="Adres e-mail do kontaktu" required>
-      
-      <label for="subject">Temat</label>
-      <input id="subject" type="text" name="subject" placeholder="Temat wiadomości" required>
-      
-      <label id="message" for="name">Wiadomość</label>
-      <textarea id="message" name="message" placeholder="Wiadomość" required></textarea>
+    <div class="container">
+      <h1>Kontakt</h1>
+      <form id="form" action="javascript:postData()" method="post">
+        <div class=form-input>
+          <label for="name">Imię i nazwisko</label>
+          <input id="name" type="text" name="name" placeholder="Imię i nazwisko" required>
+        </div>
+        
+        <div class=form-input>
+          <label for="mail">Adres E-mail</label>
+          <input id="mail" type="text" name="mail" placeholder="Adres e-mail do kontaktu" required>
+        </div>
+        
+        <div class=form-input>
+          <label for="subject">Temat</label>
+          <input id="subject" type="text" name="subject" placeholder="Temat wiadomości" required>
+        </div>
 
-      <input type="text" id="website" name="website" autocomplete='off'/> <!-- honeypot  -->
-      
-      <input type="submit">
-    </form>
+        <div class=form-input>
+          <label id="message" for="name">Wiadomość</label>
+          <textarea id="message" name="message" placeholder="Wiadomość" required></textarea>
+          <input type="text" id="website" name="website" autocomplete='off'/> <!-- honeypot  -->
+        </div>
+        <button>Wyślij</button>
+      </form>
+    </div>
+
+    <footer></footer>
   </body>
 </html>
